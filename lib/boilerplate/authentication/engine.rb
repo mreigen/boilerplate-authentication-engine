0e57f6694bfaa47efd151d325e7d26b18f635faf
Minh Reigen <mreigen@gmail.com>
2018-04-29 23:04:55 -0700

    append_migrations to call migration from the parent app

--- lib/boilerplate/authentication/engine.rb
+++ lib/boilerplate/authentication/engine.rb
@@ -3,6 +3,14 @@ module Boilerplate
     class Engine < ::Rails::Engine
       isolate_namespace Boilerplate::Authentication
       config.generators.api_only = true
+
+      initializer :append_migrations do |app|
+       unless app.root.to_s.match(root.to_s)
+         config.paths["db/migrate"].expanded.each do |p|
+           app.config.paths["db/migrate"] << p
+          end
+        end
+      end
     end
   end
 end
